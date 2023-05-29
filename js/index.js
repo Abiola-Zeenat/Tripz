a60724992688a0ff724c8f0227756d52f89e4b3a
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2023-05-29 16:07:39 +0100

    add some functionalities

--- js/index.js
+++ js/index.js
@@ -1,180 +1,190 @@
+// change likee icon color
+var Likee = document.querySelectorAll(".likee").length;
+for (let i = 0; i < Likee; i++) {
+  document.querySelectorAll(".likee")[i].addEventListener("click", likee);
+  function likee() {
+    document.querySelectorAll(".like-img1")[i].src = "../Assets/Teal-heart.png";
+    document.querySelectorAll(".like-img")[i].src = "Assets/Teal-heart.png";
+    
+  }
+}
 
 // password toggle
 
 const togglePassword = document.querySelector("#togglePassword");
-        const password = document.querySelector("#password");
+const password = document.querySelector("#password");
 
-        togglePassword.addEventListener("click", function () {
-            // toggle the type attribute
-            const type = password.getAttribute("type") === "password" ? "text" : "password";
-            password.setAttribute("type", type);
-            
-            // toggle the icon
-            this.classList.toggle("fa-eye");
-        });
- 
+togglePassword.addEventListener("click", function () {
+  // toggle the type attribute
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+
+  // toggle the icon
+  this.classList.toggle("fa-eye");
+});
+
 // change button color when filled
-        $(".form-input").on("keyup", function () {
-            if($(this).val()==''){
-            $('.cta1').css({background:'#99A1A1'})
-          }
-        else{
-            $('.cta1').css({background:'#008080'})
-          }
-        
-        });
+$(".form-input").on("keyup", function () {
+  if ($(this).val() == "") {
+    $(".cta1").css({ background: "#99A1A1" });
+  } else {
+    $(".cta1").css({ background: "#008080" });
+  }
+});
 
-        
-        // Form Validation
+// Form Validation
 
-const fullNameEl = document.querySelector('#fullName');
-const emailEl = document.querySelector('#email');
-const passwordEl = document.querySelector('#password');
-const form = document.querySelector('#access-form');
+const fullNameEl = document.querySelector("#fullName");
+const emailEl = document.querySelector("#email");
+const passwordEl = document.querySelector("#password");
+const form = document.querySelector("#access-form");
 const checkfullName = () => {
+  let valid = false;
 
-    let valid = false;
+  const min = 3,
+    max = 25;
 
-    const min = 3,
-        max = 25;
+  const fullName = fullNameEl.value.trim();
 
-    const fullName = fullNameEl.value.trim();
-
-    if (!isRequired(fullName)) {
-        showError(fullNameEl, 'fullName cannot be blank.');
-    } else if (!isBetween(fullName.length, min, max)) {
-        showError(fullNameEl, `fullName must be between ${min} and ${max} characters.`)
-    } else {
-        showSuccess(fullNameEl);
-        valid = true;
-    }
-    return valid;
+  if (!isRequired(fullName)) {
+    showError(fullNameEl, "fullName cannot be blank.");
+  } else if (!isBetween(fullName.length, min, max)) {
+    showError(
+      fullNameEl,
+      `fullName must be between ${min} and ${max} characters.`
+    );
+  } else {
+    showSuccess(fullNameEl);
+    valid = true;
+  }
+  return valid;
 };
 
-
 const checkEmail = () => {
-    let valid = false;
-    const email = emailEl.value.trim();
-    if (!isRequired(email)) {
-        showError(emailEl, 'Email cannot be blank.');
-    } else if (!isEmailValid(email)) {
-        showError(emailEl, 'Email is not valid.')
-    } else {
-        showSuccess(emailEl);
-        valid = true;
-    }
-    return valid;
+  let valid = false;
+  const email = emailEl.value.trim();
+  if (!isRequired(email)) {
+    showError(emailEl, "Email cannot be blank.");
+  } else if (!isEmailValid(email)) {
+    showError(emailEl, "Email is not valid.");
+  } else {
+    showSuccess(emailEl);
+    valid = true;
+  }
+  return valid;
 };
 
 const checkPassword = () => {
-    let valid = false;
+  let valid = false;
 
+  const password = passwordEl.value.trim();
 
-    const password = passwordEl.value.trim();
+  if (!isRequired(password)) {
+    showError(passwordEl, "Password cannot be blank.");
+  } else if (!isPasswordSecure(password)) {
+    showError(
+      passwordEl,
+      "Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)"
+    );
+  } else {
+    showSuccess(passwordEl);
+    valid = true;
+  }
 
-    if (!isRequired(password)) {
-        showError(passwordEl, 'Password cannot be blank.');
-    } else if (!isPasswordSecure(password)) {
-        showError(passwordEl, 'Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)');
-    } else {
-        showSuccess(passwordEl);
-        valid = true;
-    }
-
-    return valid;
+  return valid;
 };
 
 const checkConfirmPassword = () => {
-    let valid = false;
-    // check confirm password
-    const confirmPassword = confirmPasswordEl.value.trim();
-    const password = passwordEl.value.trim();
+  let valid = false;
+  // check confirm password
+  const confirmPassword = confirmPasswordEl.value.trim();
+  const password = passwordEl.value.trim();
 
-    if (!isRequired(confirmPassword)) {
-        showError(confirmPasswordEl, 'Please enter the password again');
-    } else if (password !== confirmPassword) {
-        showError(confirmPasswordEl, 'The password does not match');
-    } else {
-        showSuccess(confirmPasswordEl);
-        valid = true;
-    }
+  if (!isRequired(confirmPassword)) {
+    showError(confirmPasswordEl, "Please enter the password again");
+  } else if (password !== confirmPassword) {
+    showError(confirmPasswordEl, "The password does not match");
+  } else {
+    showSuccess(confirmPasswordEl);
+    valid = true;
+  }
 
-    return valid;
+  return valid;
 };
 
 const isEmailValid = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 };
 
 const isPasswordSecure = (password) => {
-    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
-    return re.test(password);
+  const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
+  return re.test(password);
 };
 
-const isRequired = value => value === '' ? false : true;
-const isBetween = (length, min, max) => length < min || length > max ? false : true;
-
+const isRequired = (value) => (value === "" ? false : true);
+const isBetween = (length, min, max) =>
+  length < min || length > max ? false : true;
 
 const showError = (input, message) => {
-    // get the form-field element
-    const formField = input.parentElement;
-    // add the error class
-    formField.classList.remove('success');
-    formField.classList.add('error');
+  // get the form-field element
+  const formField = input.parentElement;
+  // add the error class
+  formField.classList.remove("success");
+  formField.classList.add("error");
 
-    // show the error message
-    const error = formField.querySelector('.errorsmall');
-    error.textContent = message;
+  // show the error message
+  const error = formField.querySelector(".errorsmall");
+  error.textContent = message;
 };
 
 const showSuccess = (input) => {
-    // get the form-field element
-    const formField = input.parentElement;
+  // get the form-field element
+  const formField = input.parentElement;
 
-    // remove the error class
-    formField.classList.remove('error');
-    formField.classList.add('success');
+  // remove the error class
+  formField.classList.remove("error");
+  formField.classList.add("success");
 
-    // hide the error message
-    const error = formField.querySelector('.errorsmall');
-    error.textContent = '';
-}
+  // hide the error message
+  const error = formField.querySelector(".errorsmall");
+  error.textContent = "";
+};
 
 // to show instant error message
-form.addEventListener('input', function (e) {
-    switch (e.target.id) {
-        case 'username':
-            checkfullName();
-            break;
-        case 'email':
-            checkEmail();
-            break;
-        case 'password':
-            checkPassword();
-            break;
-            }
+form.addEventListener("input", function (e) {
+  switch (e.target.id) {
+    case "username":
+      checkfullName();
+      break;
+    case "email":
+      checkEmail();
+      break;
+    case "password":
+      checkPassword();
+      break;
+  }
 });
 
-form.addEventListener('submit', function (e) {
-    // prevent the form from submitting
-    e.preventDefault();
+form.addEventListener("submit", function (e) {
+  // prevent the form from submitting
+  e.preventDefault();
 
-    // validate fields
-    let isfullNameValid = checkfullName(),
-        isEmailValid = checkEmail(),
-        isPasswordValid = checkPassword(),
-        isConfirmPasswordValid = checkConfirmPassword();
+  // validate fields
+  let isfullNameValid = checkfullName(),
+    isEmailValid = checkEmail(),
+    isPasswordValid = checkPassword(),
+    isConfirmPasswordValid = checkConfirmPassword();
 
-    let isFormValid = isfullNameValid &&
-        isEmailValid &&
-        isPasswordValid &&
-        isConfirmPasswordValid;
+  let isFormValid =
+    isfullNameValid &&
+    isEmailValid &&
+    isPasswordValid &&
+    isConfirmPasswordValid;
 
-    // submit to the server if the form is valid
-    if (isFormValid) {
-
-    }
+  // submit to the server if the form is valid
+  if (isFormValid) {
+  }
 });
-
-
